3eb45f8354acd4e8901b1eb6108dadec580df852
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-02-03 15:13:29 +0300

    implemented dynamic content in dashboard

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,6 @@
-export default function Customers() {
+export default async function Customers() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
     return (
         <div>
         Customers
