159bef038dee59cd3928aef10a72784cba67faec
Gilbert Ssenyonjo <gssenyonjo@gmail.com>
2024-02-03 23:54:13 +0300

    Setup webpage metadata. Completed nextjs.com/learn course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: {
+        template: '%s | Dashboard',
+        default: 'Dashboard'
+    },
+    description: 'The best dashboard ever!',
+    metadataBase: new URL('http://localhost:3000')
+}
+
 
 export default function RootLayout({
   children,
